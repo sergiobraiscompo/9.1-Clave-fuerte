8ed2b7e835034bf89dca2eccac7911a2592a845d
sergiobraiscompo <comesanapoloniosergiobrais@gmail.com>
2024-08-15 14:18:19 +0200

    refactor de los test

--- src/valida-clave.spec.ts
+++ src/valida-clave.spec.ts
@@ -3,10 +3,11 @@ import { validarClave } from "./valida-clave";
 
 describe('validarClave', () => {    
     it.each([
-        [ "Legumbres", "SOA", {esValida: false}],
-        [ "Perfume", "20", {esValida: false, error: "La clave no es válida."} ],
-        [ "Leche", "Slipknot", {esValida: true} ],
-        [ "Lasaña", "asdsf", {esValida: false, error: "La clave no es válida."} ]
+        [ "user1", "SOA", {esValida: false}],
+        [ "myself", "1234567", {esValida: false, error: "La clave no es válida."} ],
+        [ "otheruser", "otheruser", {esValida: false, error: "La clave no es válida."} ],
+        [ "lagertha", "Th3_Futur3_I5_0p3n", {esValida: false, error: "La clave no es válida."} ]
+        // [ "Lasaña", "asdsf", {esValida: false, error: "La clave no es válida."} ]
     ]) ("Si el nombre de usuario es %s, y la contraseña es %s, el programa debería de devolver %s", (nombreUsuario: string, clave: string, resultadoEsperado) => {
         // Act
         const resultado = validarClave(nombreUsuario, clave, commonPasswords);
