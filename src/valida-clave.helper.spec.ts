e9ab73fb562e6ec7ad03f39e6e864ef99f196383
sergiobraiscompo <comesanapoloniosergiobrais@gmail.com>
2024-08-15 22:40:25 +0200

    tests creados

--- src/valida-clave.helper.spec.ts
+++ src/valida-clave.helper.spec.ts
@@ -3,10 +3,10 @@ import { tieneCaracteresEspeciales, tieneLongitudMinima, tieneMayusculasYMinuscu
 
 describe('tieneMayusculasYMinusculas', () => {    
     it.each([
-        [ "2", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ],
-        [ "20", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ],
-        [ "1", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ],
-        [ "5", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ]
+        [ "PASS", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ],
+        [ "51651321", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ],
+        [ "prueba", {esValida: false, error: "La clave tiene que contener mayúsculas y minúsculas."} ],
+        [ "ContraseÑa", {esValida: true} ]
     ]) ("Si la contraseña es %s, el programa debería de devolver %s", (clave: string, resultadoEsperado) => {
         // Act
         const resultado = tieneMayusculasYMinusculas(clave);
@@ -18,11 +18,9 @@ describe('tieneMayusculasYMinusculas', () => {
 
 describe('tieneNumeros', () => {    
     it.each([
-        [ "2", {esValida: false, error: "La clave tiene que contener números."} ],
-        [ "20", {esValida: false, error: "La clave tiene que contener números."} ],
-        [ "1", {esValida: false, error: "La clave tiene que contener números."} ],
-        [ "5", {esValida: false, error: "La clave tiene que contener números."} ]
-    ]) ("Si la contraseña es %s, el programa debería de devolver %s", (clave: string, resultadoEsperado) => {
+        [ "cuarentaysiete", {esValida: false, error: "La clave tiene que contener números."} ],
+        [ "Xwyu7", {esValida: true} ],
+     ]) ("Si la contraseña es %s, el programa debería de devolver %s", (clave: string, resultadoEsperado) => {
         // Act
         const resultado = tieneNumeros(clave);
         
@@ -33,10 +31,9 @@ describe('tieneNumeros', () => {
 
 describe('tieneCaracteresEspeciales', () => {    
     it.each([
-        [ "2", {esValida: false, error: "La clave ha de contener símbolos especiales."} ],
-        [ "20", {esValida: false, error: "La clave ha de contener símbolos especiales."} ],
-        [ "1", {esValida: false, error: "La clave ha de contener símbolos especiales."} ],
-        [ "5", {esValida: false, error: "La clave ha de contener símbolos especiales."} ]
+        [ "25465d5sfasd4f5", {esValida: false, error: "La clave ha de contener símbolos especiales."} ],
+        [ "C0ntrasegn@", {esValida: true} ],
+        [ "37491183", {esValida: false, error: "La clave ha de contener símbolos especiales."} ],
     ]) ("Si la contraseña es %s, el programa debería de devolver %s", (clave: string, resultadoEsperado) => {
         // Act
         const resultado = tieneCaracteresEspeciales(clave);
@@ -50,8 +47,7 @@ describe('tieneLongitudMinima', () => {
     it.each([
         [ "12345678", { esValida: true } ],
         [ "20sadfkjlfkasdjfl", { esValida: true } ],
-        [ "1", {esValida: false, error: "La clave ha de tener una longitud mínima de 8 carácteres."} ],
-        [ "5", {esValida: false, error: "La clave ha de tener una longitud mínima de 8 carácteres."} ]
+        [ "1234567", {esValida: false, error: "La clave ha de tener una longitud mínima de 8 carácteres."} ]
     ]) ("Si la contraseña es %s, el programa debería de devolver %s", (clave: string, resultadoEsperado) => {
         // Act
         const resultado = tieneLongitudMinima(clave);
@@ -65,7 +61,7 @@ describe('tieneNombreUsuario', () => {
     it.each([
         [ "user1", "ssdfa", {esValida: true} ],
         [ "pacman", "pacman", {esValida: false, error: "La clave no debe contener el nombre del usuario."} ],
-        [ "tales", "sadf", {esValida: true }],
+        [ "tales", "mileto", {esValida: true }],
         [ "mia", "123mia", {esValida: false, error: "La clave no debe contener el nombre del usuario."} ]
     ]) ("Si la contraseña es %s, el programa debería de devolver %s", (nombreUsuario: string, clave: string, resultadoEsperado) => {
         // Act
@@ -78,10 +74,10 @@ describe('tieneNombreUsuario', () => {
 
 describe('tienePalabrasComunes', () => {    
     it.each([
-        [ "2", {esValida: false, error: "La clave no debe tener palabras comunes."} ],
+        [ "password", {esValida: false, error: "La clave no debe tener palabras comunes."} ],
         [ "20", {esValida: false, error: "La clave no debe tener palabras comunes."} ],
-        [ "1", {esValida: false, error: "La clave no debe tener palabras comunes."} ],
-        [ "5", {esValida: false, error: "La clave no debe tener palabras comunes."} ]
+        [ "kingslayer", {esValida: true} ],
+        [ "qwerty", {esValida: false, error: "La clave no debe tener palabras comunes."} ]
     ]) ("Si la contraseña es %s, el programa debería de devolver %s", (clave: string, resultadoEsperado) => {
         // Act
         const resultado = tienePalabrasComunes(clave, commonPasswords);
--- src/valida-clave.spec.ts
+++ src/valida-clave.spec.ts
@@ -4,9 +4,9 @@ import { validarClave } from "./valida-clave";
 describe('validarClave', () => {    
     it.each([
         [ "user1", "SOA", {esValida: false}],
-        [ "myself", "1234567", {esValida: false, error: "La clave no es válida."} ],
-        [ "otheruser", "otheruser", {esValida: false, error: "La clave no es válida."} ],
-        [ "lagertha", "Th3_Futur3_I5_0p3n", {esValida: false, error: "La clave no es válida."} ]
+        // [ "myself", "1234567", {esValida: false, error: "La clave no es válida."} ],
+        // [ "otheruser", "otheruser", {esValida: false, error: "La clave no es válida."} ],
+        // [ "lagertha", "Th3_Futur3_I5_0p3n", {esValida: false, error: "La clave no es válida."} ]
         // [ "Lasaña", "asdsf", {esValida: false, error: "La clave no es válida."} ]
     ]) ("Si el nombre de usuario es %s, y la contraseña es %s, el programa debería de devolver %s", (nombreUsuario: string, clave: string, resultadoEsperado) => {
         // Act
